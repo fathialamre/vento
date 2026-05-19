8b8f0bb11d7369357a0fcb8a20ff903f792b7916
fathialamre <estigarebya@gmail.com>
2026-05-19 23:24:29 +0200

    feat(body): add tauri-plugin-dialog for multipart file picker

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -336,6 +336,7 @@ pub fn run() {
 
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_dialog::init())
         .plugin(
             tauri_plugin_sql::Builder::default()
                 .add_migrations("sqlite:vento.db", migrations)
